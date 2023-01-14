67622c10f0ec0fc4a886945eeacb21af891ca5d1
SukkaW <isukkaw@gmail.com>
2023-01-14 21:49:28 +0800

    fix: re-enable cache for style9 loader

--- src/loader.ts
+++ src/loader.ts
@@ -45,17 +45,6 @@ export default async function style9Loader(this: any, input: string, inputSource
     } else if (metadata?.style9 === undefined) {
       this.callback(null, input, inputSourceMap);
     } else {
-      // Webpack Virtual Module plugin doesn't support triggering a rebuild for webpack5,
-      // which can cause "module not found" error when webpack5 cache is enabled.
-      // Currently the only "non-hacky" workaround is to mark this module as non-cacheable.
-      //
-      // See also:
-      // - https://github.com/sysgears/webpack-virtual-modules/issues/86
-      // - https://github.com/sysgears/webpack-virtual-modules/issues/76
-      // - https://github.com/windicss/windicss-webpack-plugin/blob/bbb91323a2a0c0f880eecdf49b831be092ccf511/src/loaders/virtual-module.ts
-      // - https://github.com/sveltejs/svelte-loader/pull/151
-      this.cacheable(false);
-
       const cssPath = loaderUtils.interpolateName(this, virtualFileName as string, {
         content: metadata.style9
       });
