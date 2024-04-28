6910d51f6a45eca0847d514d1df0410314253dd6
SukkaW <isukkaw@gmail.com>
2024-04-28 14:26:41 +0800

    refactor: prefer `Uint8Array` over `Buffer` when possible

--- src/lib/deserialize-css.ts
+++ src/lib/deserialize-css.ts
@@ -4,13 +4,23 @@ import { SERIALIZE_COMPRESSION_FLAG } from './constants';
 
 const unzip = promisify(gunzip);
 
+const decoder = new TextDecoder();
+
 export async function deserializeCss(source: string) {
   if (source.startsWith(SERIALIZE_COMPRESSION_FLAG)) {
     const decompressedSource = await unzip(
-      Buffer.from(source.substring(SERIALIZE_COMPRESSION_FLAG.length), 'base64')
+      base64ToUint8Array(source.substring(SERIALIZE_COMPRESSION_FLAG.length))
     );
 
     return decompressedSource.toString('utf-8');
   }
-  return Buffer.from(source, 'base64').toString('utf-8');
+  return decoder.decode(base64ToUint8Array(source));
+}
+
+function base64ToUint8Array(base64String: string) {
+  return Uint8Array.from(globalThis.atob(base64UrlToBase64(base64String)), x => x.codePointAt(0)!);
+}
+
+function base64UrlToBase64(base64url: string) {
+  return base64url.replaceAll('-', '+').replaceAll('_', '/');
 }
--- src/lib/serialize-css.ts
+++ src/lib/serialize-css.ts
@@ -7,11 +7,39 @@ const zip = promisify(gzip);
 // The byte threshold for applying compression, below which compressing would out-weigh its value.
 const compressionThreshold = 1000;
 
+const encoder = new TextEncoder();
+
 export async function serializeCss(source: string) {
   if (source.length > compressionThreshold) {
     const compressedSource = await zip(source);
     return SERIALIZE_COMPRESSION_FLAG + compressedSource.toString('base64');
   }
 
-  return Buffer.from(source, 'utf-8').toString('base64');
+  return uint8ArrayToBase64(encoder.encode(source));
 }
+
+// Reference: https://phuoc.ng/collection/this-vs-that/concat-vs-push/
+const MAX_BLOCK_SIZE = 65_535;
+
+function uint8ArrayToBase64(array: Uint8Array) {
+  let base64;
+
+  if (array.length < MAX_BLOCK_SIZE) {
+    // Required as `btoa` and `atob` don't properly support Unicode: https://developer.mozilla.org/en-US/docs/Glossary/Base64#the_unicode_problem
+    base64 = globalThis.btoa(String.fromCodePoint(...array));
+  } else {
+    base64 = '';
+    for (const value of array) {
+      base64 += String.fromCodePoint(value);
+    }
+
+    base64 = globalThis.btoa(base64);
+  }
+
+  return base64ToBase64Url(base64);
+}
+
+function base64ToBase64Url(base64: string) {
+  return base64.replaceAll('+', '-').replaceAll('/', '_').replace(/=+$/, '');
+}
+
